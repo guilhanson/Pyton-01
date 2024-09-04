bd71112193f2725ee1d27cd5b523df3002ab86b7
GH <guil.hanson@gmail.com>
2024-09-04 16:39:21 -0400

    Add Main.py novamente

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+#Variaveis
+idade = 38
+nome = "GH"
+
+print(idade)
+print(nome)
+
+#Soma
+soma = idade +2
+print(soma)
+
+#Lista
+lista = [1, 2, 3, 4, 5]
+print (lista)
+print (lista.reverse())
